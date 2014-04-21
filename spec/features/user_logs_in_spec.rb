bf93ab280caae1cdf936b272ea3c6e9cdd8d7f9a
Angela Yen <ayen10@gmail.com>
2014-04-21 10:48:10 -0700

    refactor capybara test for user login with utilities helper

--- spec/features/user_logs_in_spec.rb
+++ spec/features/user_logs_in_spec.rb
@@ -3,20 +3,12 @@ require 'spec_helper'
 describe User do
   it ' lets the visitor visit the homepage' do
     visit root_path
-    page.should have_content "Welcome JanBNB"
+    page.should have_content "Welcome to janBnB"
   end
 
-  it 'lets a visitor sign up' do
-    visit root_path
-    click_link "Sign up"
-    fill_in "Name", :with => "Joe"
-    fill_in "Email", :with => "joe@email.com"
-    fill_in "Bio", :with => "I'm trustworthy."
-    choose "YES"
-    fill_in "Password", :with => "password"
-    fill_in "Password confirmation", :with => "password"
-    click_button "Sign up"
-    page.should have_content "You have signed up successfully"
+  it 'allows a visitor to create a new account as an owner and log in' do
+    create_owner_and_log_in
+    page.should have_content "Signed in"
   end
 
 end
